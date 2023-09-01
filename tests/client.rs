c9c132bbdca4dd73955627e385a53af239748056
Kezhu Wang <kezhuw@gmail.com>
2023-09-01 19:27:17 +0800

    Rename test method to_configuration to configuration

--- tests/client.rs
+++ tests/client.rs
@@ -44,7 +44,7 @@ struct Cluster {
 }
 
 impl Cluster {
-    fn to_configuration(&self) -> Configuration {
+    fn configuration(&self) -> Configuration {
         Configuration::new(self.service_uri.clone()).bookies(self.bookie_addrs.clone())
     }
 }
@@ -83,7 +83,7 @@ async fn create_empty_ledger(client: &BookKeeper) -> LedgerId {
 async fn test_ledger_open() {
     let cluster = start_bookkeeper_cluster();
 
-    let config = cluster.to_configuration();
+    let config = cluster.configuration();
     let client = BookKeeper::new(config).await.unwrap();
 
     let ledger_id = create_empty_ledger(&client).await;
@@ -104,7 +104,7 @@ async fn test_ledger_open() {
 async fn test_ledger_recover() {
     let cluster = start_bookkeeper_cluster();
 
-    let config = cluster.to_configuration();
+    let config = cluster.configuration();
     let client = BookKeeper::new(config).await.unwrap();
 
     let ledger = client.create_ledger(CREATE_OPTIONS.clone()).await.unwrap();
@@ -122,7 +122,7 @@ async fn test_ledger_recover() {
 async fn test_ledger_read() {
     let cluster = start_bookkeeper_cluster();
 
-    let config = cluster.to_configuration();
+    let config = cluster.configuration();
     let client = BookKeeper::new(config).await.unwrap();
 
     let mut ledger = client.create_ledger(CREATE_OPTIONS.clone()).await.unwrap();
@@ -210,7 +210,7 @@ async fn assert_ledger_entries<T: AsRef<[u8]>>(
 async fn test_ledger_delete() {
     let cluster = start_bookkeeper_cluster();
 
-    let config = cluster.to_configuration();
+    let config = cluster.configuration();
     let client = BookKeeper::new(config).await.unwrap();
 
     let ledger_id = create_empty_ledger(&client).await;
